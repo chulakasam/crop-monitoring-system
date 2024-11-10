6fdc8b55c6c73e3040ea8778379ebad37a214a53
chulakasam <sampathchulaka33872@gmail.com>
2024-11-10 16:29:27 +0530

    add field dao

---
+++ src/main/java/com/example/Proposed/Crop/monitoring/system/Dao/FieldDao.java
@@ -0,0 +1,11 @@
+package com.example.Proposed.Crop.monitoring.system.Dao;
+
+import com.example.Proposed.Crop.monitoring.system.Entity.Impl.FieldEntity;
+import com.example.Proposed.Crop.monitoring.system.Entity.Impl.VehicleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FieldDao extends JpaRepository<FieldEntity,String> {
+
+}
