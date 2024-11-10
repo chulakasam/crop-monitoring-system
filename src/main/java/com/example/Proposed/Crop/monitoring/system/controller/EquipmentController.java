196357e54396ff56f2c4e54af004adb1314e4875
chulakasam <sampathchulaka33872@gmail.com>
2024-11-10 16:54:18 +0530

    add equipment controller

--- src/main/java/com/example/Proposed/Crop/monitoring/system/controller/EquipmentController.java
+++ src/main/java/com/example/Proposed/Crop/monitoring/system/controller/EquipmentController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/vehicles")
-public class VehicleController {
+@RequestMapping("api/v1/equipment")
+public class EquipmentController {
 }
