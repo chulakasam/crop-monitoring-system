84a8f11e5a639f9f2caacfd2c7dea6faef0945f9
chulakasam <sampathchulaka33872@gmail.com>
2024-11-22 12:42:25 +0530

    add crossorigin for staff controller

--- src/main/java/com/example/Proposed/Crop/monitoring/system/controller/StaffController.java
+++ src/main/java/com/example/Proposed/Crop/monitoring/system/controller/StaffController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/staff")
+@CrossOrigin(origins = "http://localhost:63342")
 public class StaffController {
 @Autowired
     private StaffService staffService;
