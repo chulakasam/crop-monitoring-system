c4392213454a32d6906ccd73838ebb1c89bdf142
chulakasam <sampathchulaka33872@gmail.com>
2024-12-02 12:50:44 +0530

    JWTConfigerFilter class create

--- src/main/java/com/example/Proposed/Crop/monitoring/system/config/JWTConfigFilter.java
+++ src/main/java/com/example/Proposed/Crop/monitoring/system/config/JWTConfigFilter.java
@@ -1,4 +1,20 @@
 package com.example.Proposed.Crop.monitoring.system.config;
 
-public class JWTConfigFilter {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Configuration
+@RequiredArgsConstructor
+public class JWTConfigFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, jakarta.servlet.FilterChain filterChain) throws ServletException, IOException {
+
+
+    }
 }
