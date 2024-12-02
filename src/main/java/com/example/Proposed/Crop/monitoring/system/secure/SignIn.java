d7ab9dacb98f676c5e3f283edf532aa30c637a48
chulakasam <sampathchulaka33872@gmail.com>
2024-12-02 13:04:51 +0530

    add secure packges

---
+++ src/main/java/com/example/Proposed/Crop/monitoring/system/secure/SignIn.java
@@ -0,0 +1,16 @@
+package com.example.Proposed.Crop.monitoring.system.secure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+
+public class SignIn {
+    private String email;
+    private String password;
+}
